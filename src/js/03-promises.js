0374746a37456fee7bd1fb89bfc2d5576f1874d5
KostBoh <bohdan.kostya@gmail.com>
2024-02-04 00:38:29 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        const reason = 'Error message';
+        reject(reason);
+      }
+    }, delay);
+  });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const firstDelay = Number(formData.get('delay'));
+    const step = Number(formData.get('step'));
+    const amount = Number(formData.get('amount'));
+
+    for (let i = 1; i <= amount; i++) {
+      createPromise(i, firstDelay + (i - 1) * step)
+        .then(({ position, delay }) => {
+          iziToast.show({
+            title: 'Success',
+            message: `Promise ${position} completed after ${delay}ms`,
+            color: 'green',
+          });
+        })
+        .catch(reason => {
+          iziToast.show({
+            title: 'Error',
+            message: `Promise rejected. Reason: ${reason}`,
+            color: 'red',
+          });
+        });
+    }
+  });
+});
